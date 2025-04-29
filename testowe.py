f6c8077edf528dcdb2c234fb4bd2623e74f90314
chansghost <howdy1wevibin@gmail.com>
2025-04-29 09:47:32 +0200

    dodanie tla

--- testowe.py
+++ testowe.py
@@ -1,31 +1,27 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow,QPushButton, QLabel, QWidget, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow,QPushButton, QLabel, QWidget, QVBoxLayout
 from PyQt6.QtCore import QSize,Qt
-from PyQt6.QtGui import QPixmap
-
-height=820
-width=390
-
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
         self.setWindowTitle("apka")
-        self.setFixedSize(QSize(width,height))
-        self.setStyleSheet("QWidget { background-image: url('study_store/background.png'); background-repeat: no-repeat; background-position: center; }")
-
-       # self.logo.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        
+        self.setFixedSize(QSize(300,400))
         self.study_store()
-
        
 
+    def button_clicked_act(self):
+        self.button.setText("nacisnales mnie ala")
+        self.label.setText("<h1>to bolalo ty brutalu</h1>")
+
     def study_store(self):
-        layout = QGridLayout()
+        layout = QVBoxLayout()
         self.label = QLabel("<h1>study store</h1>",alignment=Qt.AlignmentFlag.AlignHCenter)
-       # layout.addWidget(self.logo)
+        self.button = QPushButton("kliknij")
+        self.button.clicked.connect(self.button_clicked_act)
+        
         layout.addWidget(self.label)
+        layout.addWidget(self.button)
         window = QWidget()
         window.setLayout(layout)
         
