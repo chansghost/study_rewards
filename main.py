baba0a00630d32b8f49d5c74761ca465725f72f2
chansghost <howdy1wevibin@gmail.com>
2025-04-29 10:07:08 +0200

    poczatki pracy nad ui

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow,QPushButton, QLabel, QWidget, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow,QPushButton, QLabel, QWidget, QGridLayout,QVBoxLayout
 from PyQt6.QtCore import QSize,Qt
 from PyQt6.QtGui import QPixmap
 
 height=820
 width=390
+path="study_store/"
 
 
 
@@ -14,18 +15,20 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("apka")
         self.setFixedSize(QSize(width,height))
         self.setStyleSheet("QWidget { background-image: url('study_store/background.png'); background-repeat: no-repeat; background-position: center; }")
-
-       # self.logo.setAlignment(Qt.AlignmentFlag.AlignCenter)
         
         self.study_store()
 
-       
 
     def study_store(self):
-        layout = QGridLayout()
-        self.label = QLabel("<h1>study store</h1>",alignment=Qt.AlignmentFlag.AlignHCenter)
-       # layout.addWidget(self.logo)
-        layout.addWidget(self.label)
+        layout = QVBoxLayout()
+        layout.setContentsMargins(25,50,25,50)
+        layout.setSpacing(150)
+        #self.label = QLabel("<h1>study store</h1>",alignment=Qt.AlignmentFlag.AlignHCenter)
+        self.header=QLabel()
+        self.image=QPixmap(path+"studystore.png")#later, instead of an image, there will be text to improve quality
+        self.header.setPixmap(self.image)
+        self.header.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        layout.addWidget(self.header)
         window = QWidget()
         window.setLayout(layout)
         
